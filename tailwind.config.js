440a68353512426cfa8b424f685c0d36599824e8
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2023-01-26 06:54:48 -0500

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      spacing: {
+        5: "1.25rem",
+        7: "1.75rem",
+        9: "2.25rem",
+        11: "2.75rem",
+        13: "3.25rem",
+        14: "3.5rem",
+        15: "3.75rem",
+        17: "4.25rem",
+        18: "4.5rem",
+        19: "4.75rem",
+        20: "5rem",
+        21: "5.25rem",
+        22: "5.5rem",
+        23: "5.75rem",
+        25: "6.25rem",
+        26: "6.5rem",
+        27: "6.75rem",
+        29: "7.25rem",
+        30: "7.5rem",
+        31: "7.75rem",
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+};
